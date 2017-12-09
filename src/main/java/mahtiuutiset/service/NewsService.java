73da73d14f625af43115227f181455623bdc8802
Inka L <inka.lappalainen@hotmail.com>
2017-12-10 00:56:16 +0200

    säätöä

--- src/main/java/mahtiuutiset/service/NewsService.java
+++ src/main/java/mahtiuutiset/service/NewsService.java
@@ -18,6 +18,9 @@ public class NewsService {
     private NewsObjectRepository newsRepo;
 
     public Page<NewsObject> findNewest(int howMany) {
+        if (howMany >= newsRepo.findAll().size()) {
+            howMany = newsRepo.findAll().size();
+        }
         Pageable pageable = PageRequest.of(0, howMany, Sort.Direction.DESC, "date");
         return newsRepo.findAll(pageable);
     }
@@ -45,6 +48,9 @@ public class NewsService {
     }
 
     private Object findMostPopular(int howMany) {
+        if (howMany >= newsRepo.findAll().size()) {
+            howMany = newsRepo.findAll().size();
+        }
         Pageable pageable = PageRequest.of(0, howMany, Sort.Direction.DESC, "views");
         return newsRepo.findAll(pageable);
     }
